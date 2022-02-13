63a807e0c426850158c73d405d28432b6ea87175
CovaciRao <covaci.cristian1229@yahoo.com>
2022-02-13 16:06:44 +0200

    add components and containers

---
+++ src/containers/whatModernWebsite/WhatModernWebsite.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatmodernwebsite.css';
+
+const WhatModernWebsite = () => {
+  return (
+    <div>
+        WhatModernWebsite
+    </div>
+  )
+}
+
+export default WhatModernWebsite
